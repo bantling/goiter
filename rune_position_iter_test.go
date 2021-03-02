04911a87d614f1a7db7f662d5dcb47712ec64351
Greg Hall <greghall@bantling.me>
2021-03-02 17:36:53 -0400

    Added RunePositionIter

--- rune_position_iter_test.go
+++ rune_position_iter_test.go
@@ -13,7 +13,6 @@ func TestRunePositionIter(t *testing.T) {
 	var (
 		text        = "line 1\rline 2\nline3\r\nline44"
 		lines       = []string{"line 1", "line 2", "line3", "line44"}
-		maxPos      = []int{6, 6, 5, 6}
 		iter        = NewRunePositionIter(strings.NewReader(text))
 		char        rune
 		lineNum     = 0
@@ -24,7 +23,7 @@ func TestRunePositionIter(t *testing.T) {
 	for iter.Next() {
 		if char = iter.Value(); char == '\n' {
 			assert.Equal(t, lines[lineNum], lineText.String())
-			assert.Equal(t, maxPos[lineNum], lastCharPos)
+			assert.Equal(t, len(lines[lineNum]), lastCharPos)
 			lineNum++
 			assert.Equal(t, lineNum+1, iter.Line())
 
@@ -37,5 +36,5 @@ func TestRunePositionIter(t *testing.T) {
 
 	assert.Equal(t, len(lines)-1, lineNum)
 	assert.Equal(t, len(lines), iter.Line())
-	assert.Equal(t, maxPos[len(maxPos)-1], iter.Position())
+	assert.Equal(t, len(lines[len(lines)-1]), iter.Position())
 }
